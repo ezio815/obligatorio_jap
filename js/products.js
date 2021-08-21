0f97b2059d101d374cbf56e82470564c152838d4
Zendhel <zendhelros@gmail.com>
2021-08-21 00:37:43 -0300

    "HTML products actualizado"

--- js/products.js
+++ js/products.js
@@ -1,6 +1,13 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
+
 document.addEventListener("DOMContentLoaded", function (e) {
+    // getJSONData(PRODUCTS_URL).then((res) => {
+    //     if (res.status === "ok") {
+    //         console.log(res.data)
+    //     }
+    // })
+
 
 });
