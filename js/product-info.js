5120dbb15b644a1a23b11825935a14e5f1dc9977
Ezi <exio78@hotmail.com>
2021-09-08 09:09:39 -0300

    productInfo

--- js/product-info.js
+++ js/product-info.js
@@ -2,5 +2,41 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(PRODUCT_INFO_URL).then(function(resultObj){
+        if (resultObj.status === "ok") {
+            product = resultObj.data;
+            let productNameHTML = document.getElementById("productName");
+            let productDescriptionHTML = document.getElementById("productDescription");
+            let productCurrencyHTML = document.getElementById("productCurrency");
+            let productCostHTML = document.getElementById("productCost");
+            let productSoldCountHTML = document.getElementById("productSoldCount");
+            let productCategoryHTML = document.getElementById("productCategory");
 
+            productNameHTML.innerHTML = product.name;
+            productDescriptionHTML.innerHTML = product.description;
+            productCurrencyHTML.innerHTML = product.currency;
+            productCostHTML.innerHTML = product.cost;
+            productSoldCountHTML.innerHTML = product.soldCount;
+            productCategoryHTML.innerHTML = product.category;
+
+            showImagesGallery(product.images);
+        }
+    });
 });
+
+const showImagesGallery = (array) => {
+    let htmlContentToAppend = "";
+
+    for (let i = 0; i < array.length; i++) {
+        let imageSrc = array[i];
+
+        htmlContentToAppend += `
+        <div class="col-lg-3 col-md-4 col-6">
+            <div class="d-block mb-4 h-100">
+                <img class="img-fluid img-thumbnail" src="` + imageSrc + `" alt="">
+            </div>
+        </div>
+        `
+        document.getElementById("productImagesGallery").innerHTML = htmlContentToAppend;
+    }
+}
