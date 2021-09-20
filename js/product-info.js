79582d4bc8a074ed49cdccd5fd485adb9ca8a879
Ezi <exio78@hotmail.com>
2021-09-19 23:18:58 -0300

    product-info funcional

--- js/product-info.js
+++ js/product-info.js
@@ -88,7 +88,28 @@ const addComment = () => {
         document.getElementsByName("puntaje").forEach(element => {
             if (element.checked) puntuacion = element.value;});
         let fecha = fechaActual();
-        alert(fecha);
+        let comentarios = document.getElementById("comments");
+        let score = "";
+        for (let j = 1; j <= 5; j++) {
+            if (j <= puntuacion) {
+                score += `
+                <label class="float-right star checked">★</label>
+                `
+            }
+            else {
+                score += `
+                <label class="float-right star">★</label>
+                `
+            }
+        }
+        if (comentario && puntuacion > 0) {
+            comentarios.innerHTML = `
+            <dt>${user} ${score}</dt>
+            <dd>${comentario}</dd>
+            <dd><small>Publicado el ${fecha}</small></dd>
+            <hr> ${comentarios.innerHTML}`;
+        }
+        else alert("Debes escribir un comentario y dejar tu puntuación.");
     }
     else {
         alert("Debes iniciar sesión para dejar un comentario");
@@ -98,15 +119,18 @@ const addComment = () => {
 const fechaActual = () => {
     let fecha = new Date;
     let anio = fecha.getFullYear();
-    let mes = fecha.getMonth();
+    let mes = fecha.getMonth() + 1;
     let dia = fecha.getDate();
     let hora = fecha.getHours();
     let minuto = fecha.getMinutes();
     let segundo = fecha.getSeconds();
     let lista = [anio, mes, dia, hora, minuto, segundo]
-    lista.forEach(element => {
-        if (parseInt(element) < 10) element = `0${element}`;
-    });
+    for (let i = 0; i < 6; i++) {
+        if (lista[i] < 10) lista[i] = `0${lista[i]}`;
+    }
+    // lista.forEach(element => {
+    //     if (parseInt(element) < 10) element = `0${element}`;
+    // });
     
     return `${lista[0]}-${lista[1]}-${lista[2]} ${lista[3]}:${lista[4]}:${lista[5]}`;
 }
